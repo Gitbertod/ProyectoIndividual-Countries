7ae2c0c6b3405b3bdec9b5c13e039d29c4b22b4a
Gilberto Díaz <diazp.gilberto@gmail.com>
2023-12-02 13:01:51 -0500

    detail of each country are rendered

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 export const GET_COUNTRIES = "GET_COUNTRIES"
 export const GET_DETAIL = "GET_DETAIL"
 
@@ -12,15 +11,21 @@ export const getCountries = async (dispatch) => {
     })
 }
 
-export const getDetail = async (dispatch) => {
-    const { data } = await axios.get('http://localhost:3001/countries')
+export const getDetail = async (dispatch, id) => {
+    const { data } = await axios.get(`http://localhost:3001/countries/${id}`)
     return dispatch({
         type: GET_DETAIL,
         payload: data
-    })
+    });
+};
 
-
-}
+// export const getDetail = async (dispatch) => {
+//     const { data } = await axios.get(`http://localhost:3001/countries/${id}`)
+//     return dispatch({
+//         type: GET_DETAIL,
+//         payload: data
+//     })
+// }
 
 export default { 
     getCountries,
--- client/src/views/detail/Detail.jsx
+++ client/src/views/detail/Detail.jsx
@@ -1,36 +1,31 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './detail.css'
-import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
-import { useDispatch,useSelector } from 'react-redux'
-import { getDetail } from '../../redux/actions/actions'
+import { useDispatch, useSelector } from 'react-redux';
+import { getDetail } from '../../redux/actions/actions';
 
 function Detail() {
     const { id } = useParams();
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const country = useSelector((state) => state.countryDetail);
-    const { nombre, Imagen_de_la_bandera, continente, capital, subregion, area, poblacion } = country
-    
-
-    
+    const { nombre, imagen_de_la_bandera, continente, capital, subregion, area, poblacion } = country;
 
     useEffect(() => {
-        getDetail(dispatch)
-    },[])
-
+        getDetail(dispatch, id); // Pasar el parámetro id
+    }, [dispatch, id]);
 
     return (
-        <>
-            <p>Nombre:{nombre}</p>
-            <p>Imagen de la bandera: {Imagen_de_la_bandera}</p>
+        <div className='detail'>  
+            <h3>Detalle de {nombre}</h3>
+            <img src={imagen_de_la_bandera}></img>
+            <p>Nombre: {nombre}</p>
             <p>Continente: {continente}</p>
-            <p>Capital:{capital}</p>
-            <p>Subregion:{subregion}</p>
-            <p>Area:{area}</p>
-            <p>Poblacion:{poblacion}</p>
-
-        </>
-    )
+            <p>Capital: {capital}</p>
+            <p>Subregion: {subregion}</p>
+            <p>Area: {area}</p>
+            <p>Poblacion: {poblacion}</p>
+        </div>
+    );
 }
 
-export default Detail
+export default Detail;
